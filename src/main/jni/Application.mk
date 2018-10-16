3b2838f375d600ffed14a3c39973c6895216234f
xybean <xybcoder@foxmail.com>
2018-10-16 17:20:17 +0800

    去除一些abi

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,12 +1,10 @@
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_CPPFLAGS += -fexceptions
 
 #For ANativeWindow support
-APP_PLATFORM = android-9
+APP_PLATFORM = android-14
 
-APP_ABI :=  armeabi \
-            armeabi-v7a \
+APP_ABI :=  armeabi-v7a \
             arm64-v8a \
-            mips \
             x86 \
             x86_64
